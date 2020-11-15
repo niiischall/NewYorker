7f18c70aad6c42f0afd2d4faeae3c2c9f02677e4
nischalNikit <mailfornischal@gmail.com>
2020-11-15 17:55:47 +0530

    Update: Implementing new styles.

--- src/components/Screens/TopStories/TopStories.js
+++ src/components/Screens/TopStories/TopStories.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Carousel from "react-multi-carousel";
 import Select from 'react-select';
-import "react-multi-carousel/lib/styles.css";
+import axios from 'axios';
 
+import "react-multi-carousel/lib/styles.css";
 import classes from './TopStories.css';
 
-const TopStories = (props) => {
-    const categories = [,
+import Spinner from '../../CommonComponents/Spinner/Spinner';
+
+const TopStories = React.memo((props) => {
+    const categories = [
         {label: "Arts", value: 'arts'},
         {label: "Automobiles", value: 'automobiles'},
         {label: 'Books', value: 'books'},
@@ -34,10 +37,59 @@ const TopStories = (props) => {
         {label: 'World', value: 'world'}
     ]
 
+    const colourStyles = {
+        control: styles => ({...styles, padding: '.85rem'}),
+        placeholder: styles => ({ 
+            ...styles, 
+            fontSize: '2rem',
+            color: 'var(--color-border-sidebar)',
+            letterSpacing: '0.05rem'
+        }),
+        option:  styles => ({...styles, color: 'var(--color-item-selected-sidebar)'})
+    }
+
     const [ selectedCategory, setSelectedCategory ] = useState(null);
+    const [ stories, setStories ]                   = useState([]);
+    const [ isLoading, setLoading ]                 = useState(false);
 
     const handleSelectedCategory = (event) => {
         setSelectedCategory(event);
+    }
+
+    const fetchStories = () => {
+        setLoading(true);
+    }
+
+    let content = null;
+
+    if(isLoading){
+        content = <Spinner />
+    }
+
+    if(stories.length === 0 && !isLoading){
+        content = (
+            <div 
+                className = {classes.contentMain}
+                style = {{alignItems: 'center'}}
+            >
+                <div className= {classes.contentMainHeadline}>
+                    <p className = {classes.contentMainHeadlineText}>
+                        Search for top stories across different sections on <span style = {{color: 'var(--color-item-sidebar)'}}>NYTimes.com</span>...
+                    </p>
+                </div>
+                <div className = {classes.contentMainPlaceholder}>
+                    <img 
+                        src       = "assets/images/undraw-treasure.svg" 
+                        className = {classes.contentPlaceholderImg}
+                        alt       = "Placeholder Logo"
+                    />
+                </div>
+            </div>
+        )
+    }
+
+    if(stories.length > 0 && !isLoading){
+        content = null;
     }
 
     return (
@@ -50,14 +102,19 @@ const TopStories = (props) => {
                     placeholder  = {"Select a Category"}
                     name         = {'category'}
                     options      = {categories}
-                    className   = {classes.searchInput} 
+                    className    = {classes.searchInput} 
+                    styles       = {colourStyles}
                 />
-                <button className = {classes.searchButton}>
+                <button 
+                    className = {classes.searchButton}
+                    onClick   = {fetchStories}
+                >
                     Search
                 </button>
             </div>
+            {content}
         </div>
     )
-}
+});
 
 export default TopStories;
